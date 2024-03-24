1dba11b181b91816015937c253598a1c0b84f41d
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2024-03-24 17:57:05 +0300

    Comments

--- client.py
+++ client.py
@@ -1,40 +1,39 @@
-from socket import *
+from socket import *  # For connections
 
 serverName = 'localhost'
-serverPort = 12000
+serverPort = 12000  # Port number of the server
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
+clientSocket = socket(AF_INET, SOCK_STREAM)  # Creating a socket object and TCP Connection
+clientSocket.connect((serverName, serverPort))  # Connecting to the server
 
-randomCity = clientSocket.recv(1024).decode()
+randomCity = clientSocket.recv(1024).decode()  # Receiving the random city from the server
 
 print("What is the plate code of", randomCity)
-
-cityNameOfUserGuess = ""
 
 while True:
     usersGuess = input("Your guess: ")
 
-    if usersGuess == "END":
+    if usersGuess == "END":  # If user enters 'END', send 'END' to the server and close the connection
         clientSocket.send(usersGuess.encode())
         clientSocket.close()
         break
 
-    clientSocket.send(usersGuess.encode())
+    clientSocket.send(usersGuess.encode())  # Sending the user's guess to the server
 
-    cityNameOfUserGuess = clientSocket.recv(1024).decode()
+    cityNameOfUserGuess = clientSocket.recv(1024).decode()  # Receiving the city name of the user's guess
 
     if randomCity != cityNameOfUserGuess:
-        if cityNameOfUserGuess == "NON_NUMERIC":
+        if cityNameOfUserGuess == "NON_NUMERIC":  # If user enters a non-numeric value
             print("You entered a non-numeric value. Game Over.")
             break
 
-        if cityNameOfUserGuess == "NOT_IN_RANGE":
+        if cityNameOfUserGuess == "NOT_IN_RANGE":  # If user enters a value out of range
             print("Number exceeds the range. Game Over.")
             break
-        print(f"You have entered the plate code of {cityNameOfUserGuess}")
-    else:
+
+        print(f"You have entered the plate code of {cityNameOfUserGuess}")  # If user enters a wrong value inform the user about their input
+    else:  # If user enters the correct value
         print("Correct!")
         break
 
-clientSocket.close()
+clientSocket.close()  # Closing the connection
--- server.py
+++ server.py
@@ -1,16 +1,15 @@
-import sys
-from socket import *
-import pandas as pd
-import random
+from socket import *  # For connections
+import pandas as pd  # For reading file from Excel
+import random  # For randomly picking the city
 
 fileName = "plate_list.xlsx"  # Name of the Excel file
 
 dataframe = pd.read_excel(fileName)
 
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)  # TCP Connection
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+serverPort = 12000  # To use unassigned port (choosing above 0-1023)
+serverSocket = socket(AF_INET, SOCK_STREAM)  # Creating a socket object and TCP Connection
+serverSocket.bind(('', serverPort))  # Binding the server socket to the specified port number
+serverSocket.listen(1)  # Setting the server to listen for incoming connections
 
 currentClientCount = 1
 
@@ -20,53 +19,55 @@ print("Server waiting for connection...")
 cityNames = dataframe["CityName"].tolist()  # Storing 'CityName' column in list
 cityNames = [s.strip() for s in cityNames]  # To clear 'xa0''s in city names
 
-plateNumbers = dataframe["PlateNumber"].tolist()
+plateNumbers = dataframe["PlateNumber"].tolist()  # Storing 'PlateNumber' column in list
 
 while True:
-    Flag = True
-    connectionSocket, addr = serverSocket.accept()
+    Flag = True  # To break the outer while loop when client enters 'END'
+    connectionSocket, addr = serverSocket.accept()  # Accepting an incoming connection
 
     print("Client connected from:   " + str(connectionSocket.getsockname()))
 
-    randomCity = random.choice(cityNames)
+    randomCity = random.choice(cityNames)  # Choosing random city to send client
 
-    indexOfRandomCity = cityNames.index(randomCity)
-    plateNumberOfRandomCity = plateNumbers[indexOfRandomCity]
+    indexOfRandomCity = cityNames.index(randomCity)  # Getting the index of the random city to get its plate number
+    plateNumberOfRandomCity = plateNumbers[indexOfRandomCity]  # Getting the plate number of the chosen random city
 
-    # DEBUG
+    # DEBUG DELETE LATER
     print(f"Selected city {randomCity}, Plate number of that city is {plateNumberOfRandomCity}")
+    # DEBUG DELETE LATER
 
-    connectionSocket.send(randomCity.encode())
+    connectionSocket.send(randomCity.encode())  # Sending the random city to the client
 
     while True:
-        usersGuess = connectionSocket.recv(1024).decode()
+        usersGuess = connectionSocket.recv(1024).decode()  # Receiving the client's guess
 
-        if usersGuess == "END":
-            Flag = False
+        if usersGuess == "END":  # If client enters 'END', break the outer while loop to end the server
+            Flag = False  # To break the outer while-loop
             break
 
         print("Received from client: " + str(usersGuess))
 
-        if usersGuess != plateNumberOfRandomCity:
-            if not usersGuess.isnumeric():
-                connectionSocket.send("NON_NUMERIC".encode())
-                currentClientCount += 1
+        if usersGuess != plateNumberOfRandomCity:  # If client's guess is not correct
+
+            if not usersGuess.isnumeric():  # If client's guess is not numeric
+                connectionSocket.send("NON_NUMERIC".encode())  # Send 'NON_NUMERIC' to the client
+                currentClientCount += 1  # Increment the client count
                 print(f"Waiting for {currentClientCount}th client connection")
                 print("Server waiting for connection...")
                 break
 
-            if not usersGuess.isnumeric() or int(usersGuess) < 1 or int(usersGuess) > 81:
-                connectionSocket.send("NOT_IN_RANGE".encode())
-                currentClientCount += 1
+            if not usersGuess.isnumeric() or int(usersGuess) < 1 or int(usersGuess) > 81:  # If client's guess is out of range
+                connectionSocket.send("NOT_IN_RANGE".encode())  # Send 'NOT_IN_RANGE' to the client
+                currentClientCount += 1  # Increment the client count
                 print(f"Waiting for {currentClientCount}th client connection")
                 print("Server waiting for connection...")
                 break
 
-            cityNameOfUserGuess = cityNames[int(usersGuess) - 1]  # -1 to get the index (1-1 to get Adana)
-            print(f"Client's input is the plate number of the city, {cityNameOfUserGuess}")
-            connectionSocket.send(cityNameOfUserGuess.strip().encode())
+            cityNameOfUserGuess = cityNames[int(usersGuess) - 1]  # Getting the name of the city that user guessed (-1 to get the index)
 
-    if not Flag:
+            connectionSocket.send(cityNameOfUserGuess.encode())  # Send the user's wrongly guessed plate number's city name to the client
+
+    if not Flag: # If client enters 'END', break the outer while loop to end the server
         break
 
-connectionSocket.close()
+connectionSocket.close()  # Closing the connection
